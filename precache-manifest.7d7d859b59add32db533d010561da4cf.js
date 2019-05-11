1b3ff155ebe119c2edbccd3406251f878a24d4e7
Tajman <tcohenhenry@umass.edu>
2019-05-11 00:18:28 -0400

    Updates

---
+++ precache-manifest.7d7d859b59add32db533d010561da4cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c7ac99d8e12d7c6a45",
+    "url": "/React-space/static/js/runtime~main.37a433e0.js"
+  },
+  {
+    "revision": "9b60ac8de3fd6f064090",
+    "url": "/React-space/static/js/main.e63ed7d6.chunk.js"
+  },
+  {
+    "revision": "91b29f88a893f2413cae",
+    "url": "/React-space/static/js/2.d337d3a2.chunk.js"
+  },
+  {
+    "revision": "9b60ac8de3fd6f064090",
+    "url": "/React-space/static/css/main.c81531c8.chunk.css"
+  },
+  {
+    "revision": "e60bf340c6e6bef382a63f7cb2b722e9",
+    "url": "/React-space/index.html"
+  }
+];
